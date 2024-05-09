0a57962b0e0689a6f8f8879d3fb42c3054f71a07
Akash Singh <aki24695@gmail.com>
2024-05-09 10:46:55 +0530

    change rust main to call desired function given in argument

--- rust_examples/src/main.rs
+++ rust_examples/src/main.rs
@@ -1,3 +1,12 @@
+mod hello; // This line imports the hello module
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let program = env::args().nth(1).expect("No program specified");
+    match program.as_str() {
+        "hello" => hello::hello(), // Calls the hello function from the hello module
+        _ => println!("Unknown program: {}", program),
+    }
 }
+
